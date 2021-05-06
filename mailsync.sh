08b655f718d4490818cc35b29bc454842d98ab4b
Marcos Guilherme <55901750+CZERNOB0G@users.noreply.github.com>
2021-05-06 17:45:26 -0300

    Create mailsync.sh

---
+++ mailsync.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+echo "Email Origem:"
+read origem
+echo "Email Destino:"
+read destino
+corrige_origem=`echo $origem | awk -F'@' '{print $2}'`;
+host=`host mbox.$corrige_origem | awk -F'address' '{print $2}' | head -n 1`;
+while [ -z "${confirmacao}" -o "${confirmacao^}" != 'N' ];
+    do
+        imapsync --host1 $host --user1 $origem --password1 'aq1sw2@#' --nossl1 --host2 $host --user2 $destino --password2 'aq1sw2@#' --nossl2
+        echo "=================================="
+        echo "Deseja migrar mais uma vez? (S/N)"
+        echo "=================================="
+        read confirmacao
+    done
+echo "==============================================="
+echo "Pode ser feito a migração inversa agora? (S/N) "
+echo "==============================================="
+read confirmacao
+if  [ "${confirmacao2^}" != 'S' ];
+    then
+        exit;
+fi
+while [ -z "${confirmacao2}" -o "${confirmacao2^}" != 'N' ];
+    do
+        imapsync --host1 $host --user1 $destino --password1 'aq1sw2@#' --nossl1 --host2 $host --user2 $origem --password2 'aq1sw2@#' --nossl2
+        echo "=================================="
+        echo "Deseja migrar mais uma vez? (S/N)"
+        echo "=================================="
+        read confirmacao2
+    done
+echo "============"
+echo " Finalizado!"
+echo "============"
+exit;
